75e05bbf2389f73720f37bfa123f76a23348b279
Konrad <konrix99@wp.pl>
2019-05-24 16:57:20 +0200

    Move funcs and add pricelist view
    Functions are moved to my_funcs.py
    Add pricelist views

---
+++ my_func.py
@@ -0,0 +1,84 @@
+import datetime
+
+from flask import request, render_template
+from sqlalchemy import func, and_
+
+from .db import db_session
+from .models import Appointment, Service, Doctor, PriceList
+
+class FormQuery:
+    """Get queries and returns data structure"""
+    def __init__(self):
+        self.doctors = db_session.query(Doctor)
+        self.services = db_session.query(Service)
+        self.payments = (
+            db_session.query(Appointment)
+                .group_by(Appointment.payment)
+        )
+
+
+def get_doctor_service_and_price(new_form):
+
+    doctor_id = (
+        db_session.query(Doctor.id)
+            .filter(Doctor.doctor_name == new_form['doctor'])
+    )
+    service_id = (
+        db_session.query(Service.id)
+            .filter(Service.name == new_form['service'])
+    )
+    price_id = (
+        db_session.query(PriceList.id).filter(and_(
+                PriceList.doctor_id == doctor_id,
+                PriceList.service_id == service_id)
+    ))
+
+    return dict(doctor_id=doctor_id, service_id=service_id, price_id=price_id)
+
+
+def query_service(tab='commercial'):
+    doctor = request.args.get('doctor_name')
+    service = request.args.get('service')
+    data = ''
+
+    data_service = db_session.query(Service)
+    data_doctor = db_session.query(Doctor)
+
+    if doctor and service:
+        doc_id = data_doctor.filter(Doctor.doctor_name == doctor).first()
+        ser_id = data_service.filter(Service.name == service).first()
+        data = (
+            db_session.query(PriceList)
+                .filter(and_(
+                PriceList.doctor_id == doc_id.id,
+                PriceList.service_id == ser_id.id
+            )).first()
+        )
+    return render_template('reception/add_new.html',
+                           data_doctor=data_doctor,
+                           data_service=data_service,
+                           doctor=doctor,
+                           service=service,
+                           data=data,
+                           page='add',
+                           tab=tab)
+
+
+def date_to_datetime(form_name, timedelta=0):
+
+    date = request.args.get(form_name)
+    print('date: ', date)
+    print('date type: ', type(date))
+
+    try:
+        date = list_date(date)
+    except (ValueError, AttributeError):
+        date = datetime.datetime.now() - datetime.timedelta(days=timedelta)
+        date = list_date(date.strftime('%Y-%m-%d'))
+        print('new date: ', date)
+    return datetime.datetime(date[0],
+                             date[1],
+                             date[2]).strftime('%s')
+
+def list_date(date):
+    return list(map(int,date.split('-')))
--- reception.py
+++ reception.py
@@ -1,10 +1,15 @@
+import datetime
+
 from flask import (
     Blueprint, abort, render_template, request, jsonify, redirect, url_for, g
 )
+from sqlalchemy import func, and_
+
 from .db import db_session
 from .models import Base, Appointment, Service, Doctor, PriceList
-import datetime
-from sqlalchemy import func, and_
+from .my_func import (
+FormQuery, get_doctor_service_and_price, query_service, date_to_datetime
+)
 
 bp = Blueprint('reception', __name__)
 
@@ -17,24 +22,38 @@ def index():
             .filter(func.strftime('%s', Appointment.date) >= start_date)
             .all()
     )
-    if visits:
-        return render_template('reception/visits.html',
+    return render_template('reception/visits.html',
                                visits=visits,
                                page='home')
-    else:
-        return 'Brak wizyt'
 
 
-@bp.route('/<int:id>', methods=('GET', 'PUT'))
+@bp.route('/<int:id>', methods=('GET', 'POST'))
 def update(id):
-    if request.method == 'PUT':
-        pass
-    else:
-        data = (
-            db_session.query(Appointment)
-                .filter(Appointment.id == id).one_or_none()
-        )
-        return render_template('reception/update.html', data=data)
+    data = (
+        db_session.query(Appointment)
+            .filter(Appointment.id == id).one_or_none()
+    )
+
+    if request.method == 'POST':
+        new_form = request.form
+        data_form = get_doctor_service_and_price(new_form)
+
+        data.patient_name = new_form['patient_name']
+        data.doctor_id = data_form['doctor_id']
+        data.service_id = data_form['service_id']
+        data.price_id = data_form['price_id']
+        data.payment=new_form['payment']
+
+        try:
+            data.referral=new_form['referral']
+            data.referral_accepted=new_form['referral_accepted']
+        except KeyError:
+            pass
+
+        db_session.add(data)
+        db_session.commit()
+
+    return render_template('reception/update.html', data=data, form=FormQuery())
 
 
 @bp.route('/nowa-wizyta-komercyjna', methods=('GET', 'POST'))
@@ -53,13 +72,11 @@ def add_commercial():
                 doctor_id=data_form['doctor_id'],
                 service_id=data_form['service_id'],
                 price_id=data_form['price_id'],
-                payment=new_form['payment']
+                payment=new_form['payment'],
             ))
-            # db_session.add(Appointment(name=name))
             db_session.commit()
             print('zapisano')
-            # return redirect(url_for('index'))
-            return f'{request.form}'
+            return redirect(url_for('index'))
     else:
         return query_service()
 
@@ -95,52 +112,14 @@ def add_pzu():
 def edit_pricelist():
     pass
 
+@bp.route('/cennik')
+def pricelist():
 
-def get_doctor_service_and_price(new_form):
-
-    doctor_id = (
-        db_session.query(Doctor.id)
-            .filter(Doctor.doctor_name == new_form['doctor'])
-    )
-    service_id = (
-        db_session.query(Service.id)
-            .filter(Service.name == new_form['service'])
-    )
-    price_id = (
-        db_session.query(PriceList.id).filter(and_(
-                PriceList.doctor_id == doctor_id,
-                PriceList.service_id == service_id)
-    ))
-
-    return dict(doctor_id=doctor_id, service_id=service_id, price_id=price_id)
-
-
-def query_service(tab='commercial'):
-    doctor = request.args.get('doctor_name')
-    service = request.args.get('service')
-    data = ''
-
-    data_service = db_session.query(Service)
-    data_doctor = db_session.query(Doctor)
-
-    if doctor and service:
-        doc_id = data_doctor.filter(Doctor.doctor_name == doctor).first()
-        ser_id = data_service.filter(Service.name == service).first()
-        data = (
-            db_session.query(PriceList)
-                .filter(and_(
-                PriceList.doctor_id == doc_id.id,
-                PriceList.service_id == ser_id.id
-            )).first()
-        )
-    return render_template('reception/add_new.html',
-                           data_doctor=data_doctor,
-                           data_service=data_service,
-                           doctor=doctor,
-                           service=service,
-                           data=data,
-                           page='add',
-                           tab=tab)
+    form_query = FormQuery()
+    pricelist = db_session.query(PriceList)
+    return render_template('reception/pricelist.html', page='price',
+                           price=pricelist, data_doctor=form_query.doctors,
+                           data_service=form_query.services)
 
 
 @bp.route('/szukaj', methods=('GET', 'POST'))
@@ -148,17 +127,15 @@ def search_data():
     if request.method == 'GET':
         name = request.args.get('name')
         dates = request.args
-        # print(dates)
-        # print('name: ', name)
+        doctor = request.args.get('doctor')
+        payment = request.args.get('payment')
         start_date = date_to_datetime('start_date', 30)
-        # print('start_date: ', start_date)
         try:
             end_date = date_to_datetime('end_date', -1)
-            # print('end_date: ', end_date)
         except:
-            # print('brak daty końcowej')
             end_date = None
 
+        # Query dates
         if end_date:
             data = (
                 db_session.query(Appointment)
@@ -173,15 +150,31 @@ def search_data():
                     .filter( func.strftime('%s', Appointment.date) >= start_date)
             )
 
+        # Query patient name
         if name:
             data = data.filter(Appointment.patient_name.like(f'{name}%'))
         else:
             name = ''
 
+        # Query doctor name
+        if doctor:
+            doctor_id = (
+                db_session.query(Doctor.id)
+                    .filter(Doctor.doctor_name.like(f'{doctor}'))
+            )
+            data = data.filter(Appointment.doctor_id == doctor_id)
+        else:
+            doctor = ''
 
+        # Query payment method
+        if payment:
+            data = data.filter(Appointment.payment.like(f'{payment}'))
+        else:
+            payment = ''
+
+        # Print extra info
         data_all = data.all()
         patient_sum = len(data_all)
-        # print(data.first().price.price)
         revenue = 0
         for i in data_all:
             try:
@@ -194,6 +187,8 @@ def search_data():
                                start_date=dates.get('start_date'),
                                end_date=dates.get('end_date'),
                                name=name,
+                               doctor=doctor,
+                               payment=payment,
                                patient_sum=patient_sum,
                                revenue=revenue,
                                page='search')
@@ -201,21 +196,3 @@ def search_data():
     return render_template('reception/search.html')
 
 
-def date_to_datetime(form_name, timedelta=0):
-
-    date = request.args.get(form_name)
-    print('date: ', date)
-    print('date type: ', type(date))
-
-    try:
-        date = list_date(date)
-    except (ValueError, AttributeError):
-        date = datetime.datetime.now() - datetime.timedelta(days=timedelta)
-        date = list_date(date.strftime('%Y-%m-%d'))
-        print('new date: ', date)
-    return datetime.datetime(date[0],
-                             date[1],
-                             date[2]).strftime('%s')
-
-def list_date(date):
-    return list(map(int,date.split('-')))
