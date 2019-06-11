7c6e1d9a16421f0082bed14fce279d8709466214
Konrad <konrad.bonder@onet.pl>
2019-06-11 17:15:57 +0200

    Move get query to FormQuery class
    Move get doctor_id service_id price_id to method in FormQuery class

--- reception.py
+++ reception.py
@@ -1,16 +1,12 @@
-import datetime
-
 from flask import (
-    Blueprint, abort, render_template, request, jsonify, redirect, url_for, g
+    Blueprint, render_template, request, redirect, url_for, flash
 )
 from sqlalchemy import func, and_
 
 from .db import db_session
-from .models import Base, Appointment, Service, Doctor, PriceList
+from .models import Appointment, PriceList
 from .auth import login_required
-from .my_func import (
-FormQuery, get_doctor_service_and_price, query_service, date_to_datetime
-)
+from .my_func import FormQuery, query_service, date_to_datetime
 
 bp = Blueprint('reception', __name__)
 
@@ -25,8 +21,8 @@ def index():
             .all()
     )
     return render_template('reception/visits.html',
-                               visits=visits,
-                               page='home')
+                           visits=visits,
+                           page='home')
 
 
 @bp.route('/<int:id>', methods=('GET', 'POST'))
@@ -39,22 +35,25 @@ def update(id):
 
     if request.method == 'POST':
         new_form = request.form
-        data_form = get_doctor_service_and_price(new_form)
+        data_form = FormQuery()
 
+        # update data
         data.patient_name = new_form['patient_name']
-        data.doctor_id = data_form['doctor_id']
-        data.service_id = data_form['service_id']
-        data.price_id = data_form['price_id']
-        data.payment=new_form['payment']
+        data.doctor_id = data_form.get_doctor_id(new_form['doctor'])
+        data.service_id = data_form.get_service_id(new_form['service'])
+        data.price_id = data_form.get_price_id()
+        data.payment = new_form['payment']
 
         try:
-            data.referral=new_form['referral']
-            data.referral_accepted=new_form['referral_accepted']
+            data.referral = new_form['referral']
+            data.referral_accepted = new_form['referral_accepted']
         except KeyError:
             pass
 
         db_session.add(data)
         db_session.commit()
+
+        flash('Zaktualizowano pomyślnie')
 
     return render_template('reception/update.html', data=data, form=FormQuery())
 
@@ -69,13 +68,14 @@ def add_commercial():
 
         if not new_form:
             err = 'This fields is required!'
+            flash(err)
         else:
-            data_form = get_doctor_service_and_price(new_form)
+            data_form = FormQuery()
             db_session.add(Appointment(
                 patient_name=new_form['patient_name'],
-                doctor_id=data_form['doctor_id'],
-                service_id=data_form['service_id'],
-                price_id=data_form['price_id'],
+                doctor_id=data_form.get_doctor_id(new_form['doctor']),
+                service_id=data_form.get_service_id(new_form['service']),
+                price_id=data_form.get_price_id(),
                 payment=new_form['payment'],
             ))
             db_session.commit()
@@ -90,11 +90,11 @@ def add_commercial():
 def add_medicover():
     if request.method == 'POST':
         new_form = request.form
-        data_form = get_doctor_service_and_price(new_form)
+        data_form = FormQuery()
         db_session.add(Appointment(
             patient_name=new_form['patient_name'],
-            doctor_id=data_form['doctor_id'],
-            service_id=data_form['service_id'],
+            doctor_id=data_form.get_doctor_id(new_form['doctor']),
+            service_id=data_form.get_service_id(new_form['service']),
             payment='Medicover',
             referral=new_form['referral'],
             referral_accepted=new_form['referral_accepted']
@@ -118,9 +118,9 @@ def add_pzu():
 def edit_pricelist():
     pass
 
+
 @bp.route('/cennik')
 def pricelist():
-
     form_query = FormQuery()
     pricelist = db_session.query(PriceList)
     return render_template('reception/pricelist.html', page='price',
@@ -138,9 +138,11 @@ def search_data():
         doctor = request.args.get('doctor')
         payment = request.args.get('payment')
         start_date = date_to_datetime('start_date', 30)
+        data_form = FormQuery()
         try:
             end_date = date_to_datetime('end_date', -1)
-        except:
+        except Exception as e:
+            print(e)
             end_date = None
 
         # Query dates
@@ -155,7 +157,7 @@ def search_data():
         else:
             data = (
                 db_session.query(Appointment)
-                    .filter( func.strftime('%s', Appointment.date) >= start_date)
+                    .filter(func.strftime('%s', Appointment.date) >= start_date)
             )
 
         # Query patient name
@@ -166,10 +168,7 @@ def search_data():
 
         # Query doctor name
         if doctor:
-            doctor_id = (
-                db_session.query(Doctor.id)
-                    .filter(Doctor.doctor_name.like(f'{doctor}'))
-            )
+            doctor_id = data_form.get_doctor_id(doctor)
             data = data.filter(Appointment.doctor_id == doctor_id)
         else:
             doctor = ''
@@ -199,8 +198,7 @@ def search_data():
                                payment=payment,
                                patient_sum=patient_sum,
                                revenue=revenue,
+                               form=FormQuery(),
                                page='search')
 
     return render_template('reception/search.html')
-
-
